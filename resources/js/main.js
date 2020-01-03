90968a88dc58bab6a0b906ee8b693b06547bef68
Héctor <hcrsalgado@gmail.com>
2020-01-03 21:28:34 +0100

    Testing fullpage js

---
+++ resources/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+        scrollHorizontally: true,
+        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff']
+	});
+
+	// //methods
+    // fullpage_api.setAllowScrolling(false);
+    
+    // var myFullpage = new fullpage('#fullpage', {
+	// 	sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff']
+	// });
+});
